9e693572e5a7cfb2dc4833538385019521a5d281
robertwwong <robertwwong@gmail.com>
2025-12-02 22:39:10 -0800

    The easy way out by Mahakaal on reddit Slow as mud.

--- Day2/main.py
+++ Day2/main.py
@@ -1,11 +1,19 @@
-
-
+import time
 from IdChecker import IDChecker
 if __name__ == '__main__':
 
     Checker = IDChecker()
+    t1s = time.perf_counter()
     Checker.run_part_1()
-    # Checker.run_part_2()
-    # Checker.check_num_ranges()
-    # Checker.run_part_2()
+    t1e = time.perf_counter()
+    print(f'part 1 took {t1e-t1s} seconds\n')
+
+    t2s = time.perf_counter()
+    Checker.run_part_2()
+    t2e = time.perf_counter()
+    print(f'part 2 took {t2e-t2s} seconds\n')   # surprisingly... this is the fastest by 3 x previous solution
+
+    t3s = time.perf_counter()
     Checker.use_regex()
+    t4e = time.perf_counter()
+    print(f'part 2 regex took {t4e-t3s} seconds')
